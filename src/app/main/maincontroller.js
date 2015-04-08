68876f3c353348faaa476c35e7ff4fca6efdf911
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-04-08 22:43:51 +0300

    only allowed symbols

--- src/app/main/maincontroller.js
+++ src/app/main/maincontroller.js
@@ -3,13 +3,10 @@
 angular.module('hw14')
   .controller('mainCtrl', function () {
     this.inputVar = undefined;
-
+    this.mask = '+38(999)999-99-99';
     this.testVar = '';
+
     this.clickChange = function () {
       this.inputVar = this.testVar;
-    };
-
-    this.check = function () {
-
     };
   });
